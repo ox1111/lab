4d815ace08f22e26b47c4ecdb5fda132c682adee
ox1111 <31942498+ox1111@users.noreply.github.com>
2024-02-29 22:23:58 +0900

    Create deb.sh

---
+++ deb.sh
@@ -0,0 +1,8 @@
+
+deb
+
+https://apt.bingner.com/debs/1200.00/
+
+http://ftp.tku.edu.tw/Linux/ArchLinux-arm/aarch64/extra/
+
+https://www.procurs.us/pool/main/iphoneos-arm64-rootless/1900/shell-cmds/
